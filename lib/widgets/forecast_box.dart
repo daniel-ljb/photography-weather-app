bf52f9883b6bfc43edda9ce7d884df14c575cf7d
AragornOfKebroyd <benjamin.tinsley54@gmail.com>
2025-05-20 21:49:46 +0100

    made dates better

--- lib/widgets/forecast_box.dart
+++ lib/widgets/forecast_box.dart
@@ -176,8 +176,23 @@ class _ForecastBoxState extends State<ForecastBox> {
     return '${time.hour.toString().padLeft(2, '0')}:00';
   }
 
+  static const months = {
+    1: "Jan",
+    2: "Feb",
+    3: "Mar",
+    4: "Apr",
+    5: "May",
+    6: "Jun",
+    7: "Jul",
+    8: "Aug",
+    9: "Sep",
+    10: "Oct",
+    11: "Nov",
+    12: "Dec",
+  };
+
   String _formatDate(DateTime time) {
-    return '${time.month}/${time.day}';
+    return '${time.day}/${months[time.month]}';
   }
 
   @override
